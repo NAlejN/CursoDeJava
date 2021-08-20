7d15befea8586cca1cc58dd958c62efeb991d1fa
Nicolas Alejandro <alejnn98@gmail.com>
2021-08-20 19:36:55 -0300

    New content

---
+++ Proyecto/Java/DeclarandoVariable.java
@@ -0,0 +1,13 @@
+
+package declarandovariable;
+
+public class DeclarandoVariable {
+
+    public static void main(String[] args) {
+        String nombre = "Nicolas";
+        int edad = 22;
+        double salario = 1000000.10;
+        boolean carnet = true;
+    }
+    
+}
